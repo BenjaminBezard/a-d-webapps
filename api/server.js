74595c1a264c45b5ef9d004ed4ac86502229bd73
Alban de Jong <alban.de-jong@epitech.eu>
2022-03-29 00:46:53 +0200

    redeploy

--- api/server.js
+++ api/server.js
@@ -57,9 +57,9 @@ function initial() {
 }
 
 // simple route
-// app.get("/", (req, res) => {
-//     res.json({ message: "Hello world!" });
-// });
+app.get("/", (req, res) => {
+    res.json({ message: "Hello world!" });
+});
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
@@ -68,13 +68,13 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "../front/build")));
 
-app.get("/", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../front/build", "index.html"));
-});
+// app.get("/", function (request, response) {
+//     response.sendFile(path.resolve(__dirname, "../front/build", "index.html"));
+// });
 
 // set port, listen for requests
 // const PORT = process.env.NODE_DOCKER_PORT || process.env.PORT || 8080;
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
