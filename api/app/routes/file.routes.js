4ad930e3efae570c9d9835f1d477bd79736ab4ec
ImBen <benjamin.bezard@epitech.eu>
2022-04-02 12:20:55 +0200

    merge conflicts resolved after stash pop

--- api/app/routes/file.routes.js
+++ api/app/routes/file.routes.js
@@ -1,19 +1,5 @@
 const controller = require("../controllers/file.controller");
 const { authJwt } = require("../middlewares");
-const multer = require("multer");
-const path = require("path");
-
-// let storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, 'uploads')
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-
-// let upload = multer({ storage: storage });
-
 
 module.exports = function(app) {
     app.use(function (req, res, next) {
